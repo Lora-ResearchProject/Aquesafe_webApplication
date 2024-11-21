80fca1d5b7704c077dd7f81b134b6de05d2293ab
Wijekoon <sarinduwijekoon@gmail.com>
2024-11-21 20:03:57 +0530

    Project structure creation and backend auth functionality implementation

---
+++ Backend/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require("cors");
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+
+//-------------------
+app.use((req, res, next) => {
+  console.log(req.method, req.path);
+  next();
+});
+//-------------------
+
+// Routes
+app.use('/api/users', userRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Internal server error', error: err.message });
+});
+
+module.exports = app;
