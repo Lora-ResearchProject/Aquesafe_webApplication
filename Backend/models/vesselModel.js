0f80d3464431e294e3460ba550bbfc7d4835035b
Wijekoon <sarinduwijekoon@gmail.com>
2024-11-24 14:50:46 +0530

    Implement register and login functions for vessels

--- Backend/models/vesselModel.js
+++ Backend/models/vesselModel.js
@@ -1,8 +1,24 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
 
 const vesselSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   vesselId: { type: String, required: true, unique: true },
   vesselName: { type: String, required: true },
 });
+
+// Hash the password before saving
+vesselSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Compare passwords for vessel login
+vesselSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model("Vessle", vesselSchema);
