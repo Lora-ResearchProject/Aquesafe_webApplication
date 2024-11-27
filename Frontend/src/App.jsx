893a8e2adbd71dd400148bc32fa6d70167431f69
Wijekoon <sarinduwijekoon@gmail.com>
2024-11-27 20:24:24 +0530

    add dummy pages for chat and routelog pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,6 +7,9 @@ import Tracker from "./Pages/Tracker";
 import DashboardLayout from "./Components/DashboardLayout";
 import MessageDataPage from "./Pages/MessageDataPage";
 import SOSPage from "./Pages/SosPage";
+import ChatPage from "./Pages/ChatPage";
+import RouteLogPage from "./Pages/RouteLogPage";
+
 
 function App() {
   return (
@@ -48,6 +51,26 @@ function App() {
             <ProtectedRoute>
               <DashboardLayout>
                 <SOSPage />
+              </DashboardLayout>
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/chat"
+          element={
+            <ProtectedRoute>
+              <DashboardLayout>
+                <ChatPage />
+              </DashboardLayout>
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/routelog"
+          element={
+            <ProtectedRoute>
+              <DashboardLayout>
+                <RouteLogPage />
               </DashboardLayout>
             </ProtectedRoute>
           }
---
+++ Frontend/src/Pages/RouteLogPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RouteLogPage = () => {
+  return (
+    <div>
+      <h1>Route Log Page</h1>
+    </div>
+  )
+}
+
+export default RouteLogPage
