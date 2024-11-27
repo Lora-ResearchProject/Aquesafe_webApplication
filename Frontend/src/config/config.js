90af95d021720020f25ad42462812cd8b1f1d95e
udanthaisuru@gmail.com <udanthaisuru@gmail.com>
2024-11-27 15:21:08 +0530

    deployment docker config

--- Frontend/src/config/config.js
+++ Frontend/src/config/config.js
@@ -1,11 +1,11 @@
 // --- For Local use
-const backendIP = "localhost";
-const backendPort = 3001
-const baseURL = `http://${backendIP}:${backendPort}`;
+// const backendIP = "localhost";
+// const backendPort = 3001
+// const baseURL = `http://${backendIP}:${backendPort}`;
 
 // --- For Production use
-// const backendIP = "";
-// const backendPort = ;
-//const baseURL = `https://${backendIP}:${backendPort}`;
+const backendIP = "144.126.211.159";
+const backendPort = 7001;
+const baseURL = `https://${backendIP}:${backendPort}`;
 
 export {baseURL};
