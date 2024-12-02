2efd1fc499b3105b6d782ca8600ac14073cd2fd9
Wijekoon <sarinduwijekoon@gmail.com>
2024-12-02 14:13:10 +0530

    implement getSosByVesselId function and modify updateSosStatus function

--- Backend/routes/sosRoutes.js
+++ Backend/routes/sosRoutes.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const {
   getAllSos,
   getSosById,
+  getSosByVesselId,
   updateSosStatus,
 } = require("../controllers/sosController");
 
 router.get("/", getAllSos); // Get all SOS records
 router.get("/:id", getSosById); // Get SOS record by ID
+router.get("/:vesselId", getSosByVesselId); // Get SOS record by vesselId
 router.patch("/:id", updateSosStatus); // Update SOS status
 
 module.exports = router;
