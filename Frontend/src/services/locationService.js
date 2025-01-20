ca362a5e8b5151d30e36b104816465c64d362617
Sarinduu <sarinduwijekoon314@gmail.com>
2025-01-20 18:06:41 +0530

    modify functions for find the vessel locations bug

--- Frontend/src/services/locationService.js
+++ Frontend/src/services/locationService.js
@@ -6,10 +6,18 @@ const GW_API_URL = baseURL + "/api/gateway";
 
 export const fetchLatestVesselLocations = async () => {
   try {
-    const response = await axios.get(`${V_API_URL}/latest-locations`);
+    const response = await axios.get(`${V_API_URL}/latestLocations`);
     return response.data; // Return the fetched data
   } catch (error) {
-    console.error("Error fetching vessel locations:", error);
+    console.error("Error fetching vessel locations:", {
+      message: error.message,
+      config: error.config, // Axios request configuration details
+      response: error.response ? {
+        status: error.response.status,
+        data: error.response.data
+      } : "No response received", // Log response details if available
+    });
+
     throw error; // Rethrow error to handle it in the component
   }
 };
@@ -21,6 +29,22 @@ export const fetchLatestGateWayLocations = async () => {
   } catch (error) {
     console.error("Error fetching gateway locations:", error);
     throw error; // Rethrow error to handle it in the component
+  }
+};
+
+
+//---------- For testing purpose
+
+export const getAllVesselLocationsdirect = async () => {
+  try {
+    const response = await axios.get("http://159.223.194.167:9002/get_all_vessel_locations");
+    return response.data.data; // Return the fetched data
+  } catch (error) {
+    console.error("Error fetching all vessel locations:", {
+      message: error.message,
+      response: error.response ? error.response.data : "No response received",
+    });
+    throw error; // Rethrow error for handling elsewhere
   }
 };
 
