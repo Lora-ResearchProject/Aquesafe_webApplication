fa4849c4ffcf0f041a89e1097bab802583e042e9
udanthaisuru@gmail.com <udanthaisuru@gmail.com>
2025-01-27 16:55:25 +0530

    local config added

--- Frontend/src/config/config.js
+++ Frontend/src/config/config.js
@@ -1,11 +1,11 @@
 // --- For Local use
-// const backendIP = "localhost";
-// const backendPort = 3001
-// const baseURL = `http://${backendIP}:${backendPort}`;
+const backendIP = "localhost";
+const backendPort = 3001;
+const baseURL = `http://${backendIP}:${backendPort}`;
 
 // --- For Production use
-const backendIP = "159.223.194.167";
-const backendPort = 9001;
-const baseURL = `http://${backendIP}:${backendPort}`;
+// const backendIP = "159.223.194.167";
+// const backendPort = 9001;
+// const baseURL = `http://${backendIP}:${backendPort}`;
 
 export {baseURL};
