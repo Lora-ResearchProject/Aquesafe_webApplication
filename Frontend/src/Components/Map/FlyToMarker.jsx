38d184f0d089746924b1989d9c702207606f4616
Sarinduu <sarinduwijekoon314@gmail.com>
2025-02-05 14:30:18 +0530

    correct the location and zoom level of the maps

--- Frontend/src/Components/Map/FlyToMarker.jsx
+++ Frontend/src/Components/Map/FlyToMarker.jsx
@@ -3,7 +3,7 @@ import { useMap } from "react-leaflet";
 const FlyToMarker = ({ position }) => {
   const map = useMap();
   if (position) {
-    map.flyTo(position, 12, { duration: 1.5 });
+    map.flyTo(position, 10, { duration: 1.5 });
   }
   return null;
 };
