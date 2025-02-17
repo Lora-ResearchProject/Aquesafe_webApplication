b0515908b7e14397d4a1dd8444ddd55c130ca610
Sarinduu <sarinduwijekoon314@gmail.com>
2025-02-18 02:44:18 +0530

    add test files

---
+++ Backend/__tests__/server.test.js
@@ -0,0 +1,8 @@
+const request = require("supertest");
+const app = require("../app");
+
+test("GET /api/test should return a success message", async () => {
+  const response = await request(app).get("/api/test");
+  expect(response.statusCode).toBe(200);
+  expect(response.body.message).toBe("API is working!");
+});
