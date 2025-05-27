d5816fe3612494beb66de235c704181dd7f8e3b7
Sarinduu <sarinduwijekoon314@gmail.com>
2025-05-27 12:28:29 +0530

    zone bug fix

--- Backend/utils/vesselZone.js
+++ Backend/utils/vesselZone.js
@@ -1,14 +1,15 @@
 function isVesselInZone(vessel, zone) {
-  const { lat, lng } = vessel; // Assuming vessel has lat and lng properties
-  const polygon = zone.boundary; // Array of { lat, lng } objects
+  const { lat, lng } = vessel; // lat = Y, lng = X
+  const polygon = zone.boundary;
 
   let inside = false;
   for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
-    const xi = polygon[i].lat, yi = polygon[i].lng;
-    const xj = polygon[j].lat, yj = polygon[j].lng;
+    const xi = polygon[i].lng, yi = polygon[i].lat;
+    const xj = polygon[j].lng, yj = polygon[j].lat;
 
-    const intersect = ((yi > lat) !== (yj > lat)) &&
-      (lng < ((xj - xi) * (lat - yi)) / (yj - yi) + xi);
+    const intersect =
+      yi > lat !== yj > lat &&
+      lng < ((xj - xi) * (lat - yi)) / (yj - yi) + xi;
 
     if (intersect) inside = !inside;
   }
